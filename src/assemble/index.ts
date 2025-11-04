e343c62875a2540dd81e06ca57fe9fd72da1e64f
ｱｽﾓ <1xtelescope@gmail.com>
2025-11-05 00:07:41 +0900

    delete: all

--- src/assemble/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./metadata";
-export * from "./html";
-export * from "./parser";
-export * as xhr from "./xhr";
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./api";
-export * from "./link-to-card-plugin";
-export * from "./types";
